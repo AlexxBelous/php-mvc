cb0a1255a5038dccd63e32170f03dfcf09c1ab6a
Alex <connectionwithme1@gmail.com>
2024-11-14 12:56:47 +0200

    add plan for basicPHP___4

---
+++ Core/Db.php
@@ -0,0 +1,72 @@
+<?php
+namespace Core;
+
+use PDO;
+use PDOException;
+use PDOStatement;
+
+
+class Db
+{
+    private $connection;
+    private PDOStatement $statement;
+    private static $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public function __wakeup()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(array $db_config)
+    {
+        $dsn = "mysql:host={$db_config['host']};dbname={$db_config['dbname']};charset={$db_config['charset']};";
+        try {
+            $this->connection = new PDO($dsn, $db_config['username'], $db_config['password'], $db_config['options']);
+            return $this;
+        } catch (PDOException $e) {
+            abort(500);
+        }
+    }
+
+    public function query($query, $params = [])
+    {
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
+        return $this;
+    }
+
+    public function findAll()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+        if (!$result) {
+            abort();
+        }
+    }
+
+
+}
--- app/controllers/posts/index.php
+++ app/controllers/posts/index.php
@@ -1,3 +1,13 @@
 <?php
+use Core\Db;
+
+$db_config = require_once CONFIG . '/db.php';
+$db = Db::getInstance()->getConnection($db_config);
+
+$posts = $db->query("select * from posts order by id desc")->findAll();
+
+$resent_posts = $db->query("select * from posts order by id desc limit 3")->findAll();
+
+
 
 require_once VIEWS . '/posts/index.tpl.php';
