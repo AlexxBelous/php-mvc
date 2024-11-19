12c63904ec1d723caf2ed28e38a60dde7524dffd
AlexxBelous <connectionwithme1@gmail.com>
2024-11-19 11:20:59 +0200

    add plan for basicPHP___8

---
+++ stuff/for-8.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * return [
+ * 'required' => 'The :fieldName: field is required',
+ * 'min' => 'The :fieldName: field must be a minimum :ruleValue: characters',
+ * 'max' => 'The :fieldName: field must be a maximum :ruleValue: characters',
+ * 'email' => 'Not valid email',
+ * ];
+ */
+
+
+/**
+ * return [
+ * [
+ * 'title' => ['required' => true, 'min' => 5, 'max' => 100],
+ * 'excerpt' => ['required' => true, 'min' => 10, 'max' => 200],
+ * 'content' => ['required' => true, 'min' => 10,]
+ * ]
+ * ];
+ */
