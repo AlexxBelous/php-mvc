9dd7acd6a54a2929a7ec1c58025544a6c3f0a253
AlexxBelous <connectionwithme1@gmail.com>
2024-11-19 11:20:59 +0200

    add plan for basicPHP___8

--- app/controllers/posts/store.php
+++ app/controllers/posts/store.php
@@ -3,18 +3,39 @@
 use Core\App;
 use Core\Db;
 
-$db = App::get('Core\Db');
+$db = App::get(Db::class);
+
+$fillable = ['title', 'slug', 'excerpt', 'content'];
+$data = load($fillable);
+
+
+$errors = [];
+if (empty($data['title'])) {
+    $errors['title'] = 'Title is required';
+}
+if (empty($data['excerpt'])) {
+    $errors['excerpt'] = 'Excerpt is required';
+}
+if (empty($data['content'])) {
+    $errors['content'] = 'Content is required';
+}
+
+$slug = createSlug($data['title']);
+$data['slug'] = $slug;
+
+if (empty($errors)) {
+    if ($db->query("INSERT INTO posts(title, slug, excerpt, content) VALUES (:title, :slug, :excerpt, :content)", $data)) {
+        $_SESSION['success'] = 'Ok';
+    } else {
+        $_SESSION['error'] = 'Db error';
+    }
+    redirect('/blog');
+}
+
+require VIEWS . '/posts/create.tpl.php';
 
 
 
-$slug = createSlug($_POST['title']);
 
-$db->query("INSERT INTO posts(title, slug, excerpt, content) VALUES (:title, :slug, :excerpt, :content)", [
-    'title' => $_POST['title'],
-    'slug' => $slug,
-    'excerpt' => $_POST['excerpt'],
-    'content' => $_POST['content']
-]);
 
-header("Location: /blog");
-exit;
+
