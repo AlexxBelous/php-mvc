e24141a805197e1cdc5c7509fa7c6237545e91ec
AlexxBelous <connectionwithme1@gmail.com>
2024-12-09 07:01:20 +0200

    Video 11. 00:10:00 - 00:25:00

--- Core/Validator.php
+++ Core/Validator.php
@@ -9,7 +9,40 @@ class Validator
 
     public function validate($data = [], $rules = [])
     {
-       print_arr($data);
-       print_arr($rules);
+        foreach ($data as $fieldName => $value) {
+            if (isset($rules[$fieldName])) {
+                $this->check([
+                    'fieldName' => $fieldName,
+                    'value' => $value,
+                    'rules' => $rules[$fieldName]
+                ]);
+            }
+        }
+        return $this;
+    }
+
+    protected function check($field)
+    {
+        print_arr($field);
+    }
+
+    protected function required($value, $rule_value)
+    {
+        return !empty(trim($value));
+    }
+
+    protected function min($value, $rule_value)
+    {
+        return mb_strlen($value, 'UTF-8') >= $rule_value;
+    }
+
+    protected function max($value, $rule_value)
+    {
+        return mb_strlen($value, 'UTF-8') <= $rule_value;
+    }
+
+    protected function email($value, $rule_value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
     }
 }
